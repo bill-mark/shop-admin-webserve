e0af767c75112d110e910dfa732b6fbbd42848ae
bill.mark <2286449127@qq.com>
2021-04-22 14:14:50 +0800

    增加删除用户

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,37 +1,76 @@
 const jsonwebtoken = require('jsonwebtoken')
 const User = require('../models/users')
-const {secret} = require('../config')
+const { secret } = require('../config')
 
-class userCtl{
-    async create(ctx){
+class UserCtl {
+    async create(ctx) {
         ctx.verifyParams({  // 使用koa-parameter对参数进行校验
-            name:{type:'string',required:true},
-            password:{type:'string',required:true}
-         })
-         const {name}= ctx.request.body
-         const repeatedUser = await User.findOne({name})
-         if(repeatedUser){
-             ctx.throw(409,'用户已存在')
-         }
-         const user = await new User(ctx.request.body).save()
-         ctx.body = user
+            name: { type: 'string', required: true },
+            phone: { type: 'number', required: true },
+            password: { type: 'string', required: true }
+        })
+        const { name } = ctx.request.body
+        const repeatedUser = await User.findOne({ name })
+        if (repeatedUser) {
+            ctx.body = {
+                state:-1,
+                message:'已存在'
+            }
+            return
+        }
+        const user = await new User(ctx.request.body).save()
+        console.log(user)
+        ctx.body = {
+            state:0,
+            phone:user.phone,
+        }
     }
-    async login(ctx){
-       console.log('login')
-       
-       ctx.verifyParams({  // 使用koa-parameter对参数进行校验
-          name:{type:'string',required:true},
-          password:{type:'string',required:true}
-       })
+    async login(ctx) {
+        //console.log('login')
 
-       const user = await User.findOne(ctx.request.body)
-       if(!user){
-           ctx.throw(401,'用户或密码不正确')
-       }
-       const {_id,name} = user
-       const token = jsonwebtoken.sign({_id,name},secter,{expiresIn:'1d'})
-       ctx.body={token}
+        ctx.verifyParams({  // 使用koa-parameter对参数进行校验
+            name: { type: 'string', required: true },
+            password: { type: 'string', required: true }
+        })
+
+        const user = await User.findOne(ctx.request.body)
+        if (!user) {
+            ctx.body = {
+                state:-1,
+                message:'账户或者密码不正确'
+            }
+            return
+        }
+        const { _id, name } = user
+        const token = jsonwebtoken.sign({ _id, name }, secret, { expiresIn: '1d' })
+        ctx.body = { 
+            state:0,
+            token:token,
+            id:_id,
+            name:name
+         }
     }
+    async findall(ctx) {
+        const { per_page = 10 } = ctx.query
+        const page = Math.max(ctx.query.page * 1, 1) - 1 //乘1用来转数字  max保证不能小于1
+        const perPage = Math.max(per_page * 1, 1) //每页多少条
+        ctx.body = await User
+            .find({ name: new RegExp(ctx.query.q) })  //正则表达式模糊搜索  key-value 精确搜索
+            .limit(perPage).skip(page * perPage)
+    }
+    async delete(ctx){
+        const user = await User.findByIdAndRemove(ctx.request.body.id)
+        if(!user){
+            ctx.body = {
+                state:-1,
+                message:'目标不存在'
+            }
+            return
+        }
+        ctx.body = {
+            state:0,
+        }
+     }
 }
 
-module.exports = new userCtl()
+module.exports = new UserCtl()
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,10 +1,18 @@
 const jwt = require('koa-jwt')
 const Router = require('koa-router')
-const router = new Router({prefox:'/users'})
+const router = new Router({prefix:'/users'})
 const {secret} = require('../config')
 const auth = jwt({secret})
 
+const{create,login,findall,delete:del} = require('../controllers/users')
 
+//注册
+router.post('/create', create)
+//登录
+router.post('/login',login)
+//查找
+router.get('/findall',findall)
+//删除
+router.post('/delete',del)
 
-
-router.post('./login',login)
+module.exports = router
